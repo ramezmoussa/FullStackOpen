6c157a1855c85217ad96aab1d39b6ce7483d5e14
root <root@LAPTOP-1RPT5P6I.localdomain>
2023-01-04 13:26:38 +0200

    exercises 1.3-1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,13 +1,28 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
-  const exercises = [10, 7, 14]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
-      <Header course={course}></Header>
-      <Content parts={parts} exercises={exercises}></Content>
-      <Total exercises={exercises}></Total>
+      <Header course={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts}></Total>
     </div>
   )
 }
@@ -24,9 +39,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part={props.parts[0]} exercise={props.exercises[0]}></Part>
-      <Part part={props.parts[1]} exercise={props.exercises[1]}></Part>
-      <Part part={props.parts[2]} exercise={props.exercises[2]}></Part>
+      <Part part={props.parts[0]["name"]} exercise={props.parts[0]["exercises"]}></Part>
+      <Part part={props.parts[1]["name"]} exercise={props.parts[1]["exercises"]}></Part>
+      <Part part={props.parts[2]["name"]} exercise={props.parts[2]["exercises"]}></Part>
 
     </>
   )
@@ -47,7 +62,7 @@ const Part = (props) => {
 const Total = (props) => {
   return (
     <>
-      <p>Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
+      <p>Number of exercises {props.parts[0]["exercises"] + props.parts[1]["exercises"] + props.parts[2]["exercises"]}</p>
     </>
   )
 }
